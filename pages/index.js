e1472641d096423db239ef1d5c6e0cb9a1a0ab8c
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-11-17 15:23:15 +0100

    Scroll Image Added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import UserWebLayout from "../src/components/UserWebLayout";
-import Header from "../src/components/Header";
-import SwiperSlider from "../src/components/Swiper";
+import UserWebLayout from "../src/components/UserWebLayout"
+import Header from "../src/components/Header"
+import ImageSection from "../src/components/ImgSection"
 
 export default function Home() {
-  return (
-    <UserWebLayout webtitle="Home Page">
-      <div style={{ backgroundColor: "#0D172A" , minHeight:'100vh'}}>
-        <Header/>
-      <SwiperSlider/>
-      </div>
-    </UserWebLayout>
-  );
+    return (
+        <UserWebLayout webtitle="Home Page">
+            <div style={{ backgroundColor: "#0D172A", minHeight: "100vh" }}>
+                <Header />
+                <ImageSection />
+            </div>
+        </UserWebLayout>
+    )
 }
